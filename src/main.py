f68c565f6fa4a1461bb1d91c8ec316c5b5c4e7eb
Marcel Fox <marcelfox@live.com>
2025-04-14 13:39:35 -0300

    removing routes from main

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
 """Main module."""
 
-from fastapi import FastAPI
-
 from dotenv import load_dotenv
+from fastapi import FastAPI
 from pydantic import BaseModel
 
-
-from src.schema.tenant_schema import TenantSchema
-from src.schema.contracts_schema import ContractSchema
-from src.controllers.tenant_controller import TenantController
-from src.controllers.contract_controller import ContractController
 from src.shared.plugins import load_routes_plugin
-
-
-class HealthResponse(BaseModel):
-    message: str = "ok"
 
 
 def create_app() -> FastAPI:
@@ -26,20 +16,12 @@ def create_app() -> FastAPI:
     app = FastAPI()
     load_dotenv()
 
-    # app.add_route(router)
+    class HealthResponse(BaseModel):
+        message: str
 
     @app.get("/")
     async def health_check() -> HealthResponse:
         return HealthResponse(message="ok")
-
-    @app.post("/tenant")
-    async def tenant(tenant: TenantSchema) -> TenantSchema | None:
-        result = await TenantController().create_tenant(tenant)
-        return result
-
-    @app.post("/contract")
-    async def create_contract(contract: ContractSchema) -> ContractSchema | None:
-        return await ContractController().create_contract(contract)
 
     load_routes_plugin(app)
 
