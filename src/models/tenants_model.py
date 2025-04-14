20b9bb51bd5c7eb2c79c7dd8a0bc41589815f6d1
Marcel Fox <marcelfox@live.com>
2025-04-14 09:52:43 -0300

    Update tenants_model.py

--- src/models/tenants_model.py
+++ src/models/tenants_model.py
@@ -1,20 +1,30 @@
-from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from src.shared.declarative_base import Base
+import uuid
+from datetime import datetime
 
 
 class TenantsModel(Base):
     __tablename__ = "tenants"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    contract_id = Column(Integer, ForeignKey("usage.id"), nullable=False)
+    tenant_id = Column(
+        String, unique=True, nullable=False, default=lambda: uuid.uuid4().hex
+    )
+    email = Column(String, nullable=False)
+    contract_id = Column(Integer, ForeignKey("contracts.id"), nullable=True)
+    usage_id = Column(Integer, ForeignKey("usage.id"), nullable=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
 
-    contract = relationship("ContractsModel", back_populates="tenants")
-
-
-class TenantSchema(BaseModel):
-    id: int
-    name: str
-    contract_id: int
+    contract = relationship(
+        "ContractsModel",
+        back_populates="tenant",
+        lazy="joined",
+    )
+    usage = relationship(
+        "UsageModel",
+        back_populates="tenant",
+        lazy="joined",
+    )
