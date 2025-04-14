4dabb1d70b8d997491f6828dbbe2c7eef609f23d
Marcel Fox <marcelfox@live.com>
2025-04-14 13:29:49 -0300

    ordering imports

--- src/routes/pulse_route.py
+++ src/routes/pulse_route.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from src.schema.pulse_schema import PulseSchema
 from src.controllers.pulse_controller import PulseController
+from src.schema.pulse_schema import PulseSchema
 from src.schema.usage_schema import UsageSchema
 from src.shared.config import logger
 
 router = APIRouter()
+
 
 @router.post("/")
 async def save_pulse(pulse: PulseSchema) -> UsageSchema | None:
