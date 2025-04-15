5dc8a7233ee6f72e8503313850745b57ffbf3b59
Marcel Fox <marcelfox@live.com>
2025-04-15 13:22:12 -0300

    Refactor tenant_route to include token authentication

--- src/routes/tenant_route.py
+++ src/routes/tenant_route.py
@@ -1,25 +1,35 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from src.controllers.tenant_controller import TenantController
+from src.controllers.token_controller import TokenController
 from src.schema.tenant_schema import TenantSchema
 
 router = APIRouter()
 
 
 @router.get("/{tenant_id}")
-async def get_tenant(tenant_id: int) -> TenantSchema | None:
+async def get_tenant(
+    tenant_id: int, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> TenantSchema | None:
     return await TenantController().get_tenant(tenant_id)
 
 
 @router.post("/")
-async def create_tenant(tenant: TenantSchema) -> TenantSchema:
+async def create_tenant(
+    tenant: TenantSchema, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> TenantSchema:
     return await TenantController().create_tenant(tenant)
 
 
 @router.put("/{tenant_id}")
-async def update_tenant(tenant_id: int, tenant_data: TenantSchema) -> TenantSchema | None:
+async def update_tenant(
+    tenant_id: int, tenant_data: TenantSchema, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> TenantSchema | None:
     return await TenantController().update_tenant(tenant_id, tenant_data)
 
+
 @router.delete("/{tenant_id}")
-async def delete_tenant(tenant_id: int) -> dict:
+async def delete_tenant(tenant_id: int, token: Annotated[str, Depends(TokenController().get_current_user)]) -> dict:
     return await TenantController().delete_tenant(tenant_id)
