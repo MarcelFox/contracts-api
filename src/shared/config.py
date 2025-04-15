1005f31db2c79114350c3cd5ab51be32343122dc
Marcel Fox <marcelfox@live.com>
2025-04-15 13:21:53 -0300

    Add fake users to the database

--- src/shared/config.py
+++ src/shared/config.py
@@ -19,3 +19,22 @@ swagger_config = {
     },
     "swagger_ui_parameters": {"syntaxHighlight.theme": "obsidian"},
 }
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "tenant": "d86c3676220f4775acb15b0b403869ea",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "tenant": "d86c3676220f4775acb15b0b403869ea",
+        "disabled": True,
+    },
+}
