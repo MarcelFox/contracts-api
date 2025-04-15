cca8fb3f033e21a989e6c20d2e1fcb7202b7da33
Marcel Fox <marcelfox@live.com>
2025-04-14 23:49:33 -0300

    Update return type to ContractResponseSchema in contract route

--- src/routes/contract_route.py
+++ src/routes/contract_route.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
 from src.controllers.contract_controller import ContractController
-from src.schema.contracts_schema import ContractSchema
+from src.schema.contracts_schema import ContractSchema, ContractResponseSchema
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_contract(contract: ContractSchema) -> ContractSchema | None:
+async def create_contract(contract: ContractSchema) -> ContractResponseSchema:
     return await ContractController().create_contract(contract)
