bf644ae333c4fe4755128169a55d0404a7e292e6
Marcel Fox <marcelfox@live.com>
2025-04-15 00:54:39 -0300

    Add id, tenant_id, created_at and updated_at fields to TenantSchema

--- src/schema/tenant_schema.py
+++ src/schema/tenant_schema.py
@@ -1,9 +1,14 @@
 from typing_extensions import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 class TenantSchema(BaseModel):
+    id: Optional[int] = None
+    tenant_id: Optional[str] = None
     email: str
     contract_id: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
     class Config:
         orm_mode = True
