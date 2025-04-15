10529182919ea46db27e730fcd0caec76b396dc9
Marcel Fox <marcelfox@live.com>
2025-04-15 13:22:18 -0300

    Refactor contract_route.py to include token authentication

--- src/routes/contract_route.py
+++ src/routes/contract_route.py
@@ -1,23 +1,35 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from src.controllers.contract_controller import ContractController
-from src.schema.contracts_schema import ContractSchema, ContractResponseSchema
+from src.controllers.token_controller import TokenController
+from src.schema.contracts_schema import ContractResponseSchema, ContractSchema
 
 router = APIRouter()
 
+
 @router.get("/{contract_id}")
-async def get_contract(id: int) -> ContractResponseSchema | None:
+async def get_contract(
+    id: int, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> ContractResponseSchema | None:
     return await ContractController().get_contract(id)
 
+
 @router.post("/")
-async def create_contract(contract: ContractSchema) -> ContractResponseSchema:
+async def create_contract(
+    contract: ContractSchema, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> ContractResponseSchema:
     return await ContractController().create_contract(contract)
 
 
 @router.put("/{contract_id}")
-async def update_contract(contract_id: int, contract_data: ContractSchema) -> ContractResponseSchema | None:
+async def update_contract(
+    contract_id: int, contract_data: ContractSchema, token: Annotated[str, Depends(TokenController().get_current_user)]
+) -> ContractResponseSchema | None:
     return await ContractController().update_contract(contract_id, contract_data)
 
+
 @router.delete("/{contract_id}")
-async def delete_contract(contract_id: int) -> dict:
+async def delete_contract(contract_id: int, token: Annotated[str, Depends(TokenController().get_current_user)]) -> dict:
     return await ContractController().delete_contract(contract_id)
